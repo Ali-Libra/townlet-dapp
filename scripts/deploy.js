3291b6eb2f28ca7beafe4c73401811244d61aace
ali <chenli201709@foxmail.com>
2025-07-21 00:13:01 +0800

    提交模板

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const ContractFactory = await hre.ethers.getContractFactory("MyContract");
+  const contract = await ContractFactory.deploy();
+
+  // v6 部署后，等待交易确认要用 contract.deploymentTransaction().wait()
+  await contract.deploymentTransaction().wait();
+
+  console.log("MyContract deployed to:", contract.target);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
